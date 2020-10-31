ab1b9532b66961888a232a93164369604dc66b21
mufti123 <73735112+mufti123@users.noreply.github.com>
2020-10-31 15:30:56 +0800

    Add files via upload

---
+++ MengHitung.py
@@ -0,0 +1,157 @@
+import os
+os.system ('clear')
+print '\t<>MENGHITUNG LEWAT TERMUX<>'
+print
+print 'DI BUAT OLEH Mr.x0z'
+print
+print '1.PENJUMLAHAN'
+print '2.PENGURANGAN'
+print '3.PERKALIAN'
+print '4.PEMBAGIAN'
+print '5.PEMBAGIAN BULAT'
+print '6.PEMANGKATAN'
+print 
+pilih = input ('pilih menu : ')
+
+if pilih == 1:
+   print
+   angka_1 = input ('ANGKA PERTAMA : ')
+   angka_2 = input ('ANGKA KEDUA : ')
+   total = angka_1 + angka_2
+   print 'HASILNYA : '  
+   print (total)
+   
+elif pilih == 2:
+   print 
+   angka_1 = input ('ANGKA PERTAMA : ')
+   angka_2 = input ('ANGKA KEDUA : ')
+   total = angka_1 - angka_2
+   print 'HASILNYA : '
+   print (total)
+   
+elif pilih == 3:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 * angka_2
+	print 'HASILNYA : '
+	print (total)
+
+elif pilih == 4:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 / angka_2
+	print 'HASILNYA : '
+	print (total)
+	
+elif pilih == 5:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 // angka_2
+	print 'HASILNYA : '
+	print (total)
+	
+elif pilih == 6:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 % angka_2
+	print 'HASILNYA : '
+	print (total)
+
+else:
+    print
+    import os
+    os.system ('clear')
+    print '\teh-eh mau ngapain? '
+    print '\tmenu yang di cari ga ada'
+    print
+    print
+    print
+    print
+    print '\tKRITIK DAN SARAN : 082149008774'
+    print 
+    print '\tKEMBALI KE MENU KETIK 99 (ENTER)'
+pilih = input  (' ')
+if pilih == 99:
+     import os
+os.system ('clear')
+print '\t<>MENGHITUNG LEWAT TERMUX<>'
+print
+print 'DI BUAT OLEH Mr.x0z'
+print
+print '1.PENJUMLAHAN'
+print '2.PENGURANGAN'
+print '3.PERKALIAN'
+print '4.PEMBAGIAN'
+print '5.PEMBAGIAN BULAT'
+print '6.PEMANGKATAN'
+print 
+pilih = input ('pilih menu : ')
+
+if pilih == 1:
+   print
+   angka_1 = input ('ANGKA PERTAMA : ')
+   angka_2 = input ('ANGKA KEDUA : ')
+   total = angka_1 + angka_2
+   print 'HASILNYA : '  
+   print (total)
+   
+elif pilih == 2:
+   print 
+   angka_1 = input ('ANGKA PERTAMA : ')
+   angka_2 = input ('ANGKA KEDUA : ')
+   total = angka_1 - angka_2
+   print 'HASILNYA : '
+   print (total)
+   
+elif pilih == 3:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 * angka_2
+	print 'HASILNYA : '
+	print (total)
+
+elif pilih == 4:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 / angka_2
+	print 'HASILNYA : '
+	print (total)
+	
+elif pilih == 5:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 // angka_2
+	print 'HASILNYA : '
+	print (total)
+	
+elif pilih == 6:
+	print
+	angka_1 = input ('ANGKA PERTAMA : ')
+	angka_2 = input ('ANGKA KEDUA : ')
+	total = angka_1 % angka_2
+	print 'HASILNYA : '
+	print (total)
+
+else:
+    print
+    import os
+    os.system ('clear')
+    print '\teh-eh mau ngapain? '
+    print '\tmenu yang di cari ga ada'
+    print
+    print
+    print
+    print
+    print '\tKRITIK DAN SARAN : 082149008774'
+    print 
+    print '\tKEMBALI KE MENU KETIK 99 (ENTER)'
+pilih = input  (' ')
+if pilih == 99:
+    print '\tKETIK ULANG LAGI WOI MengHitung.py'
